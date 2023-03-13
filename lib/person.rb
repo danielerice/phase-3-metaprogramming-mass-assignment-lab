5784054093e8b26157b6471175c117d96e5bdba2
Daniel Rice <danielrice5627@gmail.com>
2023-03-13 11:14:44 -0700

    commit tree e7776ea90f12c44f1b2affae566fb0e7292fb89e

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,9 @@
 class Person
-  # your code here
+  
+  def initialize attributes_hash
+    attributes_hash.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end
+  end
 end
